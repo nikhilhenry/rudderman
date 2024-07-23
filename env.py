92af07e5c8149e1be6ba53e92ab2aa3e46201985
nikhilhenry <25770025+nikhilhenry@users.noreply.github.com>
2024-07-23 16:44:59 +0530

    create correct sized rudder

--- env.py
+++ env.py
@@ -18,33 +18,39 @@ BOUNDARY_MASK = flow.StaggeredGrid(
     BOUNDARY_BOX, flow.ZERO_GRADIENT, velocity.bounds, velocity.resolution)
 pressure = None
 
+DT = 0.5
 '''
 rudder logic:
 the boat can be a point cloud object.
 the rudder will simply act as an obstacle which spawns below the boat
 '''
-point_cloud = flow.PointCloud(flow.vec(x=5, y=5))
-# print(point_cloud.geometry.center)
-RUDDER_WIDTH = 0.5
-RUDDER_LENGTH = 5
-rudder = flow.Obstacle(flow.geom.Cuboid(
-    flow.vec(x=5, y=5), x=RUDDER_WIDTH, y=RUDDER_LENGTH))
+ORIGIN_X = 32
+ORIGIN_Y = 10
+RUDDER_WIDTH = 0.25
+RUDDER_LENGTH = 2.5
+RUDDER_LENGTH_HALF = RUDDER_LENGTH / 2
+
+point_cloud = flow.PointCloud(flow.vec(x=ORIGIN_X, y=ORIGIN_Y))
+rudder = flow.Obstacle(flow.geom.Box(
+    x=(ORIGIN_X-RUDDER_WIDTH, ORIGIN_X+RUDDER_WIDTH),
+    y=(ORIGIN_Y-RUDDER_LENGTH, ORIGIN_Y)))
 
 
-@flow.math.jit_compile
+@ flow.math.jit_compile
 def step(v, p, obj, rudder: flow.Obstacle):
-    obj = flow.advect.advect(obj, v, 1.)
+    obj = flow.advect.advect(obj, v, DT)
     # instantiate a new obstacle
     x, y = obj.geometry.center
-    rudder = rudder.at(flow.vec(x=x, y=y))
-    v = flow.advect.semi_lagrangian(v, v, 1.)
+    rudder = rudder.at(flow.vec(x=x, y=y-RUDDER_LENGTH_HALF))
+
+    v = flow.advect.semi_lagrangian(v, v, DT)
     v = v * (1 - BOUNDARY_MASK) + BOUNDARY_MASK * (SPEED, 0)
     v, p = flow.fluid.make_incompressible(
-        v, [CYLINDER], flow.Solve('auto', 1e-5, x0=p))
+        v, [CYLINDER, rudder], flow.Solve('auto', 1e-5, x0=p))
     return v, p, obj, rudder
 
 
-traj, _, obj_traj, rudder_trj = flow.iterate(step, flow.batch(time=5), velocity,
+traj, _, obj_traj, rudder_trj = flow.iterate(step, flow.batch(time=200), velocity,
                                              pressure, point_cloud, rudder, range=trange)
 anim = flow.plot([traj, CYLINDER_GEOM, rudder_trj.geometry, obj_traj], animate="time",
                  size=(20, 10), overlay="list", frame_time=50)

95e772398be16fa03721294ae4fc5d997b9b4b3d
nikhilhenry <25770025+nikhilhenry@users.noreply.github.com>
2024-07-25 01:05:07 +0530

    create gym env without render

--- env.py
+++ env.py
@@ -1,81 +1,153 @@
-"""Karman Vortex Street
-Air flow around a static cylinder.
-Vortices start appearing after a couple of hundred steps.
-"""
-
+import numpy as np
+import gymnasium as gym
+from gymnasium import spaces
 from phi.jax import flow
-from tqdm import tqdm
-from matplotlib import pyplot as plt
-
-SPEED = 2
-velocity = flow.StaggeredGrid(
-    (SPEED, 0), flow.ZERO_GRADIENT, x=128, y=128, bounds=flow.Box(x=128, y=64))
-CYLINDER_GEOM = flow.geom.infinite_cylinder(x=15, y=32, radius=10)
-CYLINDER = flow.Obstacle(CYLINDER_GEOM)
-BOUNDARY_BOX = flow.Box(x=(-1 * flow.INF, 0.5), y=None)
-BOUNDARY_MASK = flow.StaggeredGrid(
-    BOUNDARY_BOX, flow.ZERO_GRADIENT, velocity.bounds, velocity.resolution)
-pressure = None
-
-DT = 0.5
-STEPS = 10
-'''
-rudder logic:
-the boat can be a point cloud object.
-the rudder will simply act as an obstacle which spawns below the boat
-'''
-ORIGIN_X = 32
-ORIGIN_Y = 10
-RUDDER_WIDTH = 0.5
-RUDDER_LENGTH = 2.5
-RUDDER_LENGTH_HALF = RUDDER_LENGTH / 2
-RUDDER_GEOM = flow.geom.Box(x=RUDDER_WIDTH, y=RUDDER_LENGTH)
-
-point_cloud = flow.PointCloud(flow.vec(x=ORIGIN_X, y=ORIGIN_Y))
 
 
-@flow.math.jit_compile
-def step(v, p, obj, angle: float):
-    obj = flow.advect.advect(obj, v, DT)
+class KarmVortexStreetEnv(gym.Env):
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
 
-    # create the rudder at the new angle
-    x, y = obj.geometry.center
-    # convert the angle to radians
-    angle = angle * flow.math.PI / 180
-    # offset the rudder in the y-axis
-    positon = flow.vec(x=x, y=y - RUDDER_LENGTH_HALF)
-    rudder = RUDDER_GEOM.at(positon)
-    rudder = flow.geom.rotate(rudder, angle, flow.vec(x=x, y=y))
-    rudder = flow.Obstacle(rudder)
+    def __init__(self, render_mode=None):
+        self.size = (128, 64)  # length and height of the sim env
+        self.bonus = 30000
 
-    v = flow.advect.semi_lagrangian(v, v, DT)
-    v = v * (1 - BOUNDARY_MASK) + BOUNDARY_MASK * (SPEED, 0)
-    v, p = flow.fluid.make_incompressible(
-        v, [CYLINDER, rudder], flow.Solve('auto', 1e-5, x0=p))
-    return v, p, obj, rudder
+        # for flow blind boat only the relative position is provided as an observation
+        self.observation_space = spaces.Box(0, self.size[0], shape=(2,))
 
+        # continous actions space of degree of rudder rotation from 0 to 360 // should be these in radian instead? should these normalised
 
-v_traj = [velocity]
-p_traj = [point_cloud]
-r_traj = [flow.Obstacle(RUDDER_GEOM).at(
-    flow.vec(x=ORIGIN_X, y=ORIGIN_Y-RUDDER_LENGTH_HALF))]
-angle = 270 
+        self.action_space = spaces.Box(0, 360)
 
-for idx in tqdm(range(STEPS)):
-    velocity, pressure, point_cloud, rudder_new = step(
-        velocity, pressure, point_cloud, angle)
-    v_traj.append(velocity)
-    p_traj.append(point_cloud)
-    r_traj.append(rudder_new)
+        assert render_mode is None or render_mode in self.metadata["render_modes"]
+        self.render_mode = render_mode
 
-traj = flow.stack(v_traj, flow.batch("time"))
-p_traj = flow.stack(p_traj, flow.batch("time"))
-r_traj = flow.stack(r_traj, flow.batch("time"))
+        """
+        Defining the phiflow constants
+        """
+        self._SPEED = 2
+        self._DT = 0.25
+        self._OBSTACLE_DIAMETER = 10
+        BOUNDARY_BOX = flow.Box(x=(-1 * flow.INF, 0.5), y=None)
+        self._BOUNDS = flow.Box(x=self.size[0], y=self.size[1])
+        self._CYLINDER_X = 15
+        self._CYLINDER_Y = 32
+        self._CYLINDER_GEOM = flow.geom.infinite_cylinder(
+            x=self._CYLINDER_X, y=self._CYLINDER_Y, radius=self._OBSTACLE_DIAMETER / 2
+        )
+        self._CYLINDER = flow.Obstacle(self._CYLINDER_GEOM)
+        self._BOUNDARY_MASK = flow.StaggeredGrid(
+            BOUNDARY_BOX, flow.ZERO_GRADIENT, x=128, y=128, bounds=self._BOUNDS
+        )
+        # defining rudder geometry
+        RUDDER_WIDTH = 0.5
+        RUDDER_LENGTH = 2.5
+        self._RUDDER_LENGTH_HALF = RUDDER_LENGTH / 2
+        self._RUDDER_GEOM = flow.geom.Box(x=RUDDER_WIDTH, y=RUDDER_LENGTH)
 
-anim = flow.plot([traj, CYLINDER_GEOM, r_traj.geometry, p_traj],
-                 animate="time", size=(20, 10), overlay="list", frame_time=50,
-                 color=["#43a5b3", "#f07857", "#ff87ce", "#bf2C34"])
+    def _get_obs(self):
+        x, y = self._boat.geometry.center
+        return [x, y]
 
-# display the simulated results
-plt.show()
-# anim.save("outputs/rudder_control.gif", writer="pillow")
+    def _get_info(self):
+        return {
+            "distance": np.linalg.norm(
+                flow.math.numpy(
+                    flow.math.vec_length(
+                        self._boat.geometry.center - self._target_position
+                    )
+                )
+            )
+        }
+
+    def reset(self, *, seed=None, options=None):
+        super().reset(seed=seed)
+
+        self._step_count = 0
+
+        # @todo randomly sample the start and target
+        start_x = self._CYLINDER_X + 5 * self._OBSTACLE_DIAMETER
+        start_y = self._CYLINDER_Y - 2.05 * self._OBSTACLE_DIAMETER
+        target_x = self._CYLINDER_X + 5 * self._OBSTACLE_DIAMETER
+        target_y = self._CYLINDER_Y + 2.05 * self._OBSTACLE_DIAMETER
+        _start_position = flow.vec(x=start_x, y=start_y)
+        self._target_position = flow.vec(x=target_x, y=target_y)
+
+        """
+        Setting up the PhiFlow sim environment
+        """
+        self._velocity = flow.StaggeredGrid(
+            (self._SPEED, 0), flow.ZERO_GRADIENT, x=128, y=128, bounds=self._BOUNDS
+        )
+        self._pressure = None
+        self._boat = flow.PointCloud(_start_position)
+
+        self._prev_distance = 0
+
+        observation = self._get_obs()
+        info = self._get_info()
+
+        return observation, info
+
+    @flow.math.jit_compile
+    def _sim_step(self, v, p, obj, angle: float):
+        """
+        accepts angles in radians
+        """
+        obj = flow.advect.advect(obj, v, self._DT)
+        x, y = obj.geometry.center
+        positon = flow.vec(x=x, y=y - self._RUDDER_LENGTH_HALF)
+        rudder = self._RUDDER_GEOM.at(positon)
+        rudder = flow.geom.rotate(rudder, angle, flow.vec(x=x, y=y))
+        rudder = flow.Obstacle(rudder)
+
+        v = flow.advect.semi_lagrangian(v, v, self._DT)
+        v = v * (1 - self._BOUNDARY_MASK) + self._BOUNDARY_MASK * (self._SPEED, 0)
+        v, p = flow.fluid.make_incompressible(
+            v, [self._CYLINDER, rudder], flow.Solve("auto", 1e-5, x0=p)
+        )
+        return v, p, obj, rudder
+
+    def step(self, action):
+        self._step_count += 1
+
+        # convert the action angle to radians
+        angle = flow.math.degrees_to_radians(action)
+        self._velocity, self._pressure, self._boat, _ = self._sim_step(
+            self._velocity, self._pressure, self._boat, angle
+        )
+
+        observation = self._get_obs()
+        info = self._get_info()
+
+        # check to see that the boat is still in the world
+        if (
+            self._boat.geometry.center[0] <= self.size[0]
+            and self._boat.geometry.center[0] >= 0
+            and self._boat.geometry.center[1] <= self.size[1]
+            and self._boat.geometry.center[1] >= 0
+        ):
+            terminated = True
+            reward = -1 * self._step_count
+            return observation, reward, terminated, False, info
+
+        # check euclidean distance between boat's position and target
+        distance_to_target = flow.math.vec_length(
+            self._boat.geometry.center - self._target_position
+        )
+        terminated = (
+            True if distance_to_target <= self._OBSTACLE_DIAMETER / 6 else False
+        )
+        bonus = self.bonus if terminated else 0
+        reward = (
+            -1 * self._step_count
+            + 10 * (distance_to_target - self._prev_distance)
+            + bonus
+        )
+
+        if self.render_mode == "human":
+            self._render_frame(angle)
+
+        return observation, reward, terminated, False, info
+
+    def _render_frame(self, angle):
+        pass
